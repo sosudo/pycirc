91c3a187eeaec9bca7ec9880d316463b61685066
Imperial Industries <33400784+Imperial-Industries@users.noreply.github.com>
2021-01-14 21:24:09 -0500

    Create circ.py

---
+++ circ.py
@@ -0,0 +1,20 @@
+def lcirc(shift:int, bit:str) -> str:
+  bit = str(bit) if not isinstance(bit,str) else bit
+  bit_list = [i for i in bit]
+  shift_bit = ''
+  for i in range(shift):
+    bit_list.append(bit_list[0])
+    bit_list.pop(0)
+  for i in bit_list:
+    shift_bit += i
+  return shift_bit
+def rcirc(shift:int, bit:str) -> str:
+  bit = str(bit) if not isinstance(bit,str) else bit
+  bit_list = [i for i in bit][::-1]
+  shift_bit = ''
+  for i in range(shift):
+    bit_list.append(bit_list[0])
+    bit_list.pop(0)
+  for i in bit_list[::-1]:
+    shift_bit += i
+  return shift_bi
